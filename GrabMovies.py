3da859d8b2ea43be60264b72947da356c2844053
wyatthumphreys <wlhumphreys@student.rtc.edu>
2018-11-29 20:13:54 -0800

    -Can parse, working on database

--- GrabMovies.py
+++ GrabMovies.py
@@ -1,24 +1,30 @@
-import os, sqlite3, http.client
+#SOURCES: https://stackoverflow.com/questions/42445237/looping-through-a-json-array-in-python
+import mysql.connector
+import sys
+import json
+import urllib.request
+import os
+import time
 
-#conn = sqlite3.connector.connect(user='root', password='',
-                    #              host='127.0.0.1',
-                     #             database='MovieDB')
-#cursor = conn.cursor()
-#cursor.execute('''CREATE TABLE IF NOT EXISTS Movies (id INT PRIMARY KEY AUTO_INCREMENT, Vote_Average TEXT, Title TEXT, Release_Date TEXT, Description TEXT, Company TEXT, Apply_info TEXT, Salary FLOAT, RawMessage TEXT, Github_ID TEXT); ''')
 
-connect = http.client.HTTPSConnection("api.themoviedb.org")
+conn = mysql.connector.connect(user='root', password='',
+                                  host='127.0.0.1',
+                                  database='MovieDB')
+cursor = conn.cursor()
+cursor.execute('''CREATE TABLE IF NOT EXISTS Movies (id INT PRIMARY KEY AUTO_INCREMENT, Vote_Average TEXT, Title TEXT, Release_Date TEXT, Description TEXT); ''')
 
-payload = "{}"
-
-connect.request("GET", "/3/discover/movie?page=1&include_video=false&include_adult=false&sort_by=popularity.desc&language=en-US&api_key=INSERTAPIKEY", payload)
-
-#GET GENRES BELOW
-#connect.request("GET", "/3/genre/movie/list?language=en-US&api_key=INSERTAPIKEYHERE", payload)
-
-res = connect.getresponse()
-data = res.read()
-
-print(data)
+query = "https://api.themoviedb.org/3/discover/movie?page=1&include_video=false&include_adult=false&sort_by=popularity.desc&language=en-US&api_key=6a16db157e43a17b6576a257990c575f"
+jsonpage = 0
+contents = urllib.request.urlopen(query)
+response = contents.read()
+jsonpage = json.loads(response)
+for i in jsonpage['results']:
+    print(i['title'])
+    cursor.execute('''INSERT INTO Movies (Vote_Average, Title, Release_Date, Description) VALUES (
+    "''' + i['vote_average'] + '''",
+    "''' + i['title'] + '''",
+    "''' + i['release_date'] + '''",
+    "''' + i['description'] + '''")''')
 
 #GENRE LIST
 #b'{"id":28,"name":"Action"}' \
@@ -40,15 +46,3 @@ print(data)
 #'{"id":53,"name":"Thriller"}' \
 #'{"id":10752,"name":"War"}' \
 #'{"id":37,"name":"Western"}]}'
-
-
-
-for page in data:
-    print(page['title'])
-    #cursor.execute('''INSERT INTO Jobs (PostDate, Title, Location, Description, Company, Apply_info) VALUES (
-    #"''' + page['created_at'] + '''",
-    #"''' + page['title'] + '''",
-    #"''' + page['location'] + '''",
-    #"''' + page['description'] + '''",
-    #"''' + page['company'] + '''",
-    #"''' + page['how_to_apply'] + '''")''')
