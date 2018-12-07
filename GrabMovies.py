031abeaccf7f66efb51bdc7d10208c649f647dc5
wyatthumphreys <wlhumphreys@student.rtc.edu>
2018-12-06 19:49:00 -0800

    -Added Comments

--- GrabMovies.py
+++ GrabMovies.py
@@ -8,7 +8,7 @@ import urllib.request
 import os
 import time
 
-
+#Creates and setsup SQL DB. Setup so no movie can be accidently added twice.
 conn = mysql.connector.connect(user='root', password='',
                                   host='127.0.0.1',
                                   database='MovieDB',
@@ -16,11 +16,13 @@ conn = mysql.connector.connect(user='root', password='',
 cursor = conn.cursor()
 cursor.execute('''CREATE TABLE IF NOT EXISTS Movies (id INT PRIMARY KEY AUTO_INCREMENT, Vote_Average TEXT, Title TEXT, Release_Date TEXT, Overview TEXT); ''')
 
+#Query the website
 query = "https://api.themoviedb.org/3/discover/movie?page=1&include_video=false&include_adult=false&sort_by=popularity.desc&language=en-US&api_key="
 jsonpage = 0
 contents = urllib.request.urlopen(query)
 response = contents.read()
 jsonpage = json.loads(response)
+#Put the data into the DB
 for i in jsonpage['results']:
     title = i['title']
     cursor.execute("SELECT Title FROM Movies WHERE Title = '" + title + "';")
@@ -56,7 +58,7 @@ for i in jsonpage['results']:
 #'{"id":10752,"name":"War"}' \
 #'{"id":37,"name":"Western"}]}'
 
-
+#Get ready for questions
 cursor.execute("SELECT Title,vote_average FROM Movies WHERE vote_average<=5.9;")
 Q1Bad = cursor.fetchall()
 cursor.execute("SELECT Title,vote_average FROM Movies WHERE vote_average>=6.0 && vote_average<=7.0;")
@@ -64,6 +66,7 @@ Q1Good = cursor.fetchall()
 cursor.execute("SELECT Title,vote_average FROM Movies WHERE vote_average>=7.1;")
 Q1Great = cursor.fetchall()
 
+#Ask the questions
 Q1 = input("Looking for a bad movie, a good movie, or a great movie? Bad = 0, Good = 1, Great = 2 ")
 if Q1 == "0":
     print("You will be shown bad movies: ")
